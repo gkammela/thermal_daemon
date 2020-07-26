c196e7b01d900f0f02be9d70ca66614a01dffdda
Srinivas Pandruvada <srinivas.pandruvada@linux.intel.com>
2020-07-25 18:16:01 -0700

    Remove default zone acpitz

--- src/thd_engine_adaptive.cpp
+++ src/thd_engine_adaptive.cpp
@@ -1101,6 +1101,10 @@ void cthd_engine_adaptive::set_int3400_target(struct adaptive_target target) {
 		if (zone) {
 			zone->set_zone_inactive();
 		}
+		zone = search_zone("acpitz");
+		if (zone) {
+			zone->set_zone_inactive();
+		}
 
 		for (int i = 0; i < (int)psvt->psvs.size(); i++) {
 			struct psv *psv = &psvt->psvs[i];
